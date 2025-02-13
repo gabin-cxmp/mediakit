efad9afa7210df09f7a238a1e300a6ad360074b7
gabin-cxmp <gabin.touitou@comexposium.com>
2025-02-13 16:52:58 +0100

    Add files via upload

---
+++ js/bannersWithPicture.js
@@ -0,0 +1,59 @@
+import { FORMATS_BANNERS_WITH_PICTURES } from './constants.js';
+import { dom } from './constants.js';
+import { createElement } from './utils.js';
+import { createTitleSocialsContainer, createDownloadButton } from './utils.js';
+
+export const generatePictures = async (croppedImg, standLetter, standNumber) => {
+  const generatedImages = [];
+  const scaleFactor = 2;
+
+  for (const picture of FORMATS_BANNERS_WITH_PICTURES) {
+    const [originalWidth, originalHeight] = picture.dimensions;
+    const scaledWidth = originalWidth * scaleFactor;
+    const scaledHeight = originalHeight * scaleFactor;
+
+    const highResCanvas = document.createElement('canvas');
+    highResCanvas.width = scaledWidth;
+    highResCanvas.height = scaledHeight;
+    const highResCtx = highResCanvas.getContext('2d');
+
+    const templateImg = new Image();
+    templateImg.src = document.documentElement.lang === 'en' ? picture.templateEN : picture.templateFR;
+    await new Promise(resolve => templateImg.onload = resolve);
+
+    highResCtx.drawImage(templateImg, 0, 0, scaledWidth, scaledHeight);
+
+    const [pictureX, pictureY] = picture.picturePosition;
+    const [pictureWidth, pictureHeight] = picture.pictureSize;
+
+    // Draw the cropped image on the canvas
+    highResCtx.drawImage(croppedImg, pictureX * scaleFactor, pictureY * scaleFactor, pictureWidth * scaleFactor, pictureHeight * scaleFactor);
+
+    // Add text
+    highResCtx.fillStyle = '#010000';
+    highResCtx.font = `${parseInt(picture.font) * scaleFactor}px CA Mechano`;
+    highResCtx.textAlign = 'left';
+    highResCtx.fillText(`${standLetter.toUpperCase()}. ${standNumber}`, picture.standLetterPosition[0] * scaleFactor, picture.standLetterPosition[1] * scaleFactor);
+
+    const finalCanvas = document.createElement('canvas');
+    finalCanvas.width = originalWidth;
+    finalCanvas.height = originalHeight;
+    const finalCtx = finalCanvas.getContext('2d');
+    finalCtx.drawImage(highResCanvas, 0, 0, scaledWidth, scaledHeight, 0, 0, originalWidth, originalHeight);
+
+    const imageURL = finalCanvas.toDataURL('image/png');
+    const imgElement = document.createElement('img');
+    imgElement.src = imageURL;
+    imgElement.alt = `Generated ${picture.nameEN}`;
+
+    const imageContainer = document.createElement('li');
+    imageContainer.className = 'image-container';
+    const titleSocialsContainer = createTitleSocialsContainer(picture.nameEN, picture.description, picture.socials);
+    imageContainer.append(titleSocialsContainer, imgElement, createDownloadButton(imageURL, `Customized PC25${picture.nameEN}.png`));
+    dom.picturesContainer.appendChild(imageContainer);
+
+    generatedImages.push({ imageURL, fileName: `Customized Picture PC25 ${picture.nameEN}.png` });
+  }
+
+  return generatedImages;
+};
---
+++ js/customizedBanners.js
@@ -0,0 +1,57 @@
+// customizedBannerGenerator.js
+import { dom } from "./constants.js";
+import { createElement } from "./utils.js";
+import { createTitleSocialsContainer } from "./utils.js";
+import { createDownloadButton } from "./utils.js";
+
+export const generateImage = async (format, logoImg, standLetter, standNumber) => {
+    const scaleFactor = 2;
+    const [originalWidth, originalHeight] = format.dimensions;
+    const scaledWidth = originalWidth * scaleFactor;
+    const scaledHeight = originalHeight * scaleFactor;
+  
+    const highResCanvas = createElement('canvas', { width: scaledWidth, height: scaledHeight });
+    const highResCtx = highResCanvas.getContext('2d');
+  
+    try {
+      await document.fonts.load(format.font);
+    } catch (err) {
+      console.error('Font failed to load:', err);
+    }
+  
+    const templateImg = new Image();
+    templateImg.src = document.documentElement.lang === 'en' ? format.templateEN : format.templateFR;
+    await new Promise(resolve => templateImg.onload = resolve);
+  
+    highResCtx.drawImage(templateImg, 0, 0, scaledWidth, scaledHeight);
+  
+    const [logoCenterX, logoCenterY] = format.logoCenter;
+    const [maxLogoWidth, maxLogoHeight] = format.maxLogoSize;
+    const logoScaleFactor = Math.min((maxLogoWidth * scaleFactor) / logoImg.width, (maxLogoHeight * scaleFactor) / logoImg.height);
+    const scaledLogoWidth = logoImg.width * logoScaleFactor;
+    const scaledLogoHeight = logoImg.height * logoScaleFactor;
+    const logoX = logoCenterX * scaleFactor - scaledLogoWidth / 2;
+    const logoY = logoCenterY * scaleFactor - scaledLogoHeight / 2;
+  
+    highResCtx.drawImage(logoImg, logoX, logoY, scaledLogoWidth, scaledLogoHeight);
+  
+    highResCtx.fillStyle = '#010000';
+    highResCtx.font = `${parseInt(format.font) * scaleFactor}px CA Mechano`;
+    highResCtx.textAlign = 'left';
+    highResCtx.fillText(`${standLetter.toUpperCase()}. ${standNumber}`, format.standLetterPosition[0] * scaleFactor, format.standLetterPosition[1] * scaleFactor);
+  
+    const finalCanvas = createElement('canvas', { width: originalWidth, height: originalHeight });
+    const finalCtx = finalCanvas.getContext('2d');
+    finalCtx.drawImage(highResCanvas, 0, 0, scaledWidth, scaledHeight, 0, 0, originalWidth, originalHeight);
+  
+    const imageURL = finalCanvas.toDataURL('image/png');
+    const imgElement = createElement('img', { src: imageURL, alt: `Generated ${format.name}` });
+  
+    const imageContainer = createElement('li', { className: 'image-container' });
+    const titleSocialsContainer = createTitleSocialsContainer(document.documentElement.lang === 'en' ? format.nameEN : format.nameFR, format.description, format.socials);
+    imageContainer.append(titleSocialsContainer, imgElement, createDownloadButton(imageURL, `${format.nameEN} PC.png`));
+    dom.imagesContainer.appendChild(imageContainer);
+  
+    return { imageURL, fileName: `Customized Logo PC25 ${format.nameEN}.png` };
+  };
+  
