4dd6362651ea1b71d4b7006da92ceb70112831ee
gabin-cxmp <gabin.touitou@comexposium.com>
2025-02-28 10:34:08 +0100

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -10,10 +10,16 @@ document.querySelectorAll('nav a').forEach(link => {
 export const getSocialByName = (name) => SOCIALS.find(social => social.name === name);
   
 export const createElement = (tag, attributes = {}) => {
-    const element = document.createElement(tag);
-    Object.entries(attributes).forEach(([key, value]) => element[key] = value);
-    return element;
-  };
+  const element = document.createElement(tag);
+  Object.entries(attributes).forEach(([key, value]) => {
+    if (key.startsWith('data-') || !(key in element)) {
+      element.setAttribute(key, value);
+    } else {
+      element[key] = value;
+    }
+  });
+  return element;
+};
   
 export const createSocialsContainer = (socials) => {
     const container = createElement('div', { className: 'image-socials-container' });
@@ -24,8 +30,8 @@ export const createSocialsContainer = (socials) => {
     return container;
   };
   
-export const createDownloadButton = (imageURL, fileName) => 
-    createElement('a', { href: imageURL, download: fileName, textContent: 'Download', className: 'download-button-small' });
+export const createDownloadButton = (imageURL, fileName, dataId) => 
+    createElement('a', { href: imageURL, download: fileName, textContent: 'Download', className: 'download-button-small', 'data-button-id': dataId });
   
 export const createImageTitleAndDescription = (title, description) => {
     const container = createElement('div', { className: 'image-title-container' });
